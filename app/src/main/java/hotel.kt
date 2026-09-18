27f1ca52caefb1f9d2d0a6b98a4b74808bc5c3b0
Prince Ian Kirby R. Toledo <princeiankirbytoledo@gmail.com>
2026-09-18 19:14:17 +0800

    chore: add all  project assets and dependencies
    - add images
    - add json files
    - add coil and gson dependencies

---
+++ app/src/main/java/hotel.kt
@@ -0,0 +1,11 @@
+package ph.edu.comteq.toledothealpshotelapp
+
+data class hotel(
+    val hotel_id: Int,
+    val hotel_name: String,
+    val hotel_ratings: Double,
+    val hotel_to_ski_distance: Double,
+    val hotel_cover_image: String,
+    val title: String,
+    val id: Int
+)
--- app/src/main/java/ph/edu/comteq/toledothealpshotelapp/MainActivity.kt
+++ app/src/main/java/ph/edu/comteq/toledothealpshotelapp/MainActivity.kt
@@ -1,16 +1,30 @@
 package ph.edu.comteq.toledothealpshotelapp
 
+import android.R
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.gson.Gson
 import ph.edu.comteq.toledothealpshotelapp.ui.theme.ToledoTheAlpsHotelAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +34,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ToledoTheAlpsHotelAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+                    Homepage(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
@@ -31,17 +45,47 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Homepage(name: String, modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    var hotels by remember { mutableStateOf(emptyList<hotel>()) }
+
+    //load json data
+    LaunchedEffect(unit) {
+        val json = context.assets.open("hotels.json")
+            .bufferedReader()
+            .use { it.readText() }
+        val gson = Gson()
+        val hotelsArray = gson.fromJson(json, Array<Hotel>::class.java)
+        hotels = hotelsArray.toList()
+    }
+
+    Column(
+        modifier = modifier.fillMaxSize()
+    ) {
+        //Top Section
+
+        Row(
+            modifier = modifier.fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            //title
+            Text(
+                text = "The Alps Hotel",
+                fontWeight = FontWeight.Bold,
+                modifier = modifier
+            )
+
+            spacer(modifier = modifier.width(9.dp))
+        }
+    }
+
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ToledoTheAlpsHotelAppTheme {
-        Greeting("Android")
+        Homepage("Android")
     }
 }
