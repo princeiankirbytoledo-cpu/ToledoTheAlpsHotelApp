0ca30a5039365bb4c309cc277899ede8f84bcfd0
Prince Ian Kirby R. Toledo <princeiankirbytoledo@gmail.com>
2026-09-25 14:54:35 +0800

    continue to the last project adding - the pictuures icon and filter to the hotels

---
+++ app/src/main/java/ph/edu/comteq/toledothealpshotelapp/Hotel.kt
@@ -0,0 +1,9 @@
+package ph.edu.comteq.toledothealpshotelapp
+
+data class Hotel(
+    val hotel_id: Int,
+    val hotel_name: String,
+    val hotel_rating: Double,
+    val hotel_to_ski_distance: Double,
+    val hotel_cover_image: String,
+)
